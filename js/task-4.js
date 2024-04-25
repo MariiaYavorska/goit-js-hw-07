b43f6e82c035aff2674f543cedca05baae5f4e03
Mariia Yavorska <m.litvinenko16@gmail.com>
2024-04-25 08:10:02 -0400

    Create task-4.js

---
+++ js/task-4.js
@@ -0,0 +1,23 @@
+'use strict'
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', function(event) {
+
+    event.preventDefault();
+    
+    const email = event.target.elements.email.value.trim();
+    const password = event.target.elements.password.value.trim();
+    if (!email || !password) {
+    
+        alert('All form fields must be filled in');
+    } else {
+    
+        const formData = {
+            email: email,
+            password: password
+        };
+
+        console.log(formData);
+        form.reset();
+    }
+});
